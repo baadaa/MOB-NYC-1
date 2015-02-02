36326ffa4f20c4454e42b6a31b69a7c5d10f2e83
Bumhan Yu <bumhan.yu@gmail.com>
2015-02-02 02:35:06 -0500

    Completed HW #4

--- Assesments/04/Lesson04/ArrayViewController.swift
+++ Assesments/04/Lesson04/ArrayViewController.swift
@@ -8,10 +8,58 @@
 
 import UIKit
 
-class ArrayViewController: UIViewController {
+class ArrayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,  UITextFieldDelegate {
+
+    var textArray: [String] = []
+    
+    @IBOutlet var arrayTable: UITableView!
+    @IBOutlet weak var textInputField: UITextField!
+        // Reference outlets for TableView and TextField
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return textArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel.text = textArray[indexPath.row]
+        return cell
+    }   // Populate TableViewCells with Strings in textArray
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        // When Return key is tapped...
+        // If TextField is not empty, the code block runs
+        
+        if textInputField.text != "" {
+            textArray.append(textInputField.text)
+            textInputField.text = ""
+            arrayTable.reloadData()
+            // Append the Array, reload the TableView, and clear TextField for easier next input
+            
+        } else {
+            println("empty input")
+            textInputField.resignFirstResponder()
+        }   // if TextField is left empty, Array remains unchanged and keyboard is dismissed
+        
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        arrayTable.delegate = self
+        arrayTable.dataSource = self
+            // Delegate and DataSource to TableView itself
+        textInputField.delegate = self
+            // TextField delegate to self
         
         /*
         TODO one: Add a table view AND a text input box to this view controller, either in code or via the storyboard. Accept keyboard input from the text view when the return key is pressed. Add the string that was entered into the text view into an array of strings (stored in this class).
@@ -19,5 +67,6 @@ class ArrayViewController: UIViewController {
         
         */
     }
+    
 }
 
--- Assesments/04/Lesson04/MapViewController.swift
+++ Assesments/04/Lesson04/MapViewController.swift
@@ -8,10 +8,112 @@
 
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var mapTableView: UITableView!
+        // Reference outlets for two input TextFields, a message output Label and a TableView
+
+    var emailMapArray:[[String:String]] = []
+        // Array of Dictionaries to store input data
+
+    let notificationCenter = NSNotificationCenter.defaultCenter()
+        // Declare a variable for NotificationCenter
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        if textField == nameTextField {
+            if nameTextField.text == "" {
+                messageLabel.text = "Please enter name!"
+                
+            } else {
+                textField.resignFirstResponder()
+                emailTextField.becomeFirstResponder()
+            }
+                // When Return key is tapped on the first TextField, check if the TextField is empty. If so, prompt the user to enter String; otherwise, move to the next input field
+            
+        } else if textField == emailTextField {
+            if nameTextField.text != "" && emailTextField.text != "" {
+                textField.resignFirstResponder()
+                
+                emailMapArray.append([nameTextField.text:emailTextField.text])
+                mapTableView.reloadData()
+                nameTextField.text = ""
+                emailTextField.text = ""
+                
+                    // When Return key is tapped on the second TextField, and both TextFields have non-empty String values, append the Strings as Key/Value pair into the Dictionary Array, reload the TableView, and reset input TextFields
+                
+            } else {
+                messageLabel.text = "Please enter both data fields!"
+            }
+                    // If any of the two input TextFields are empty, prompt the user to enter both values.
+        }
+        
+        
+        return true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        messageLabel.text = ""
+    }   // When user start entering data in any TextField, message goes blank
+    
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emailMapArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+
+        var dict = emailMapArray[indexPath.row]
+            // Pull the Dictionary item from the Array at indexPath
+        
+        if let key = dict.keys.first {
+            // NOTE: I found this part, but not exactly clear how this works
+            
+            cell.textLabel.text = "\(key) ( \(dict[key]!) )"
+        }
+        
+        return cell
+    }   // Populate TableViewCells with Strings in Dictionary Array
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameTextField.delegate = self
+        emailTextField.delegate = self
+            // TextFields delegate to self
+        
+        mapTableView.delegate = self
+        mapTableView.dataSource = self
+            // TableView delegate and dataSource 
+        
+        notificationCenter.addObserverForName(
+            UIKeyboardDidHideNotification,
+            object: nil,
+            queue: nil,
+            usingBlock: { (notificationCenter) -> Void in
+                self.nameTextField.backgroundColor = UIColor(red: 255, green: 0, blue: 0, alpha: 0.7)
+                self.emailTextField.backgroundColor = UIColor(red: 255, green: 0, blue: 0, alpha: 0.7)
+            }
+        )   // Notification Observer for Keyboard disappearing
+        
+        notificationCenter.addObserverForName(
+            UIKeyboardWillShowNotification,
+            object: nil,
+            queue: nil,
+            usingBlock: { (notificationCenter) -> Void in
+                self.nameTextField.backgroundColor = UIColor(red: 0, green: 0, blue: 255, alpha: 0.7)
+                self.emailTextField.backgroundColor = UIColor(red: 0, green: 0, blue: 255, alpha: 0.7)
+            }
+        )   // Notification Observer for Keyboard appearing
         
         /*
         TODO three: Add TWO text views and a table view to this view controller, either using code or storybaord. Accept keyboard input from the two text views. When the 'return' button is pressed on the SECOND text view, add the text view data to a dictionary. The KEY in the dictionary should be the string in the first text view, the VALUE should be the second.
