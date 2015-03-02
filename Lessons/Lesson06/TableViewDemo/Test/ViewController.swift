e14359fb25f90a24767a6b0bb8191b7a6c521101
Bumhan Yu <bumhan.yu@gmail.com>
2015-03-02 18:41:50 -0500

    Not significant changes.

--- Lessons/Lesson06/TableViewDemo/Test/ViewController.swift
+++ Lessons/Lesson06/TableViewDemo/Test/ViewController.swift
@@ -9,15 +9,34 @@
 import UIKit
 
 class ViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var teachers = ["Rudd", "Travis", "Sean"]
 
-    let teachers = ["Rudd", "Travis", "Sean"]
-
+    var teacherTemp:[String] = [""]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NSUserDefaults.standardUserDefaults().setObject(teachers, forKey: "teachers")
+    
+        if let teacherFile = NSUserDefaults.standardUserDefaults().arrayForKey("teachers") {
+            
+           // teacherTemp += teacherFile
+        }
+    }
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let teacherFile = NSUserDefaults.standardUserDefaults().arrayForKey("teachers") {
+            
+            return teacherFile.count
+        }
+
         return teachers.count
     }
     
